91b91482b4cd7a3bb1023eeb15510b2e91c7e5c6
Zacchaeus Israel <zacchaeusisrael@Dickernss-MacBook-Pro.local>
2025-04-14 10:58:42 -0400

    Added comments, added files to structure project.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 
-// import express 
+// import express (application)
 import express from "express";
-// import axios 
+// import axios (making API calls)
 import axios from "axios";
+
+//used for parsing through requests & repsonses 
+import bodyParse from "body-parser";
 
 // create express application
 const app = express();
@@ -10,6 +13,16 @@ const app = express();
 // PORT # for application
 const port = 3000;
 
+// Set EJS as the templating engine
+app.set("view engine", "ejs");
+
+// tells EJS that all of the static files are in the public folder
+app.use(express.static("public"));
+
+// default request (when the page loads up)
+app.get('/', async (req, res) => {
+    console.log('Home page');
+});
 
 // listen for requests on port # and start the server
 app.listen(port, () => {
